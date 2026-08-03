1c4531684937e67c139de60d6010d9a5971d44d1
Hossein Barazesh <hosseinbarazesh1378@gmail.com>
2026-08-03 16:12:16 +0330

    Day 7: 24 to 12 - solved

---
+++ day-07-24-to-12/solution.py
@@ -0,0 +1,31 @@
+"""
+Challenge: 24 to 12
+Source: freeCodeCamp
+Date: 2026-08-03
+
+Given a string representing a time of the day in the 24-hour format of "HHMM",
+return the time in its equivalent 12-hour format of "H:MM AM" or "H:MM PM".
+
+The given input will always be a four-digit string in 24-hour time format,
+from "0000" to "2359".
+
+Example:
+to_12("1124") -> "11:24 AM"
+"""
+
+import datetime
+
+
+def to_12(time):
+    time = datetime.datetime.strptime(time, "%H%M")
+    return f"{int(time.strftime('%I'))}:{time.strftime('%M %p')}"
+
+
+assert to_12("1124") == "11:24 AM"
+assert to_12("0900") == "9:00 AM"
+assert to_12("1455") == "2:55 PM"
+assert to_12("2346") == "11:46 PM"
+assert to_12("0030") == "12:30 AM"
+
+print("All tests passed!")
+
